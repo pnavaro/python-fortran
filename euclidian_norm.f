4969be997e70008a6a44e35dda86fd58a459c737
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2019-10-25 12:20:53 +0200

    remove unecessary files

--- euclidian_norm.f
+++
@@ -1,5 +0,0 @@
-      subroutine euclidian_norm (a, b, c)
-      real*8 a,b,c
-Cf2py intent(out) c
-      c = sqrt (a*a+b*b) 
-      end 
--- euclidian_norm.f90
+++
@@ -1,5 +0,0 @@
-subroutine euclidian_norm (a, b, c)
-  real(8), intent(in) :: a, b
-  real(8), intent(out) :: c 
-  c =	sqrt (a*a+b*b) 
-end subroutine euclidian_norm
